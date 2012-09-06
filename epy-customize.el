252c73f3b0056fe296e02686b9bee33473d75f97
Andrey Chirkov <achirkof@gmail.com>
2012-09-06 14:30:15 +0400

    new theme

--- epy-customize.el
+++ epy-customize.el
@@ -1,11 +1,15 @@
 ;;;; Config for customization
 ;;; Color-theme
-(add-to-list 'load-path "~/.emacs.d/color-theme")
-(add-to-list 'load-path "~/.emacs.d/color-theme/themes")
-(require 'color-theme)
-(color-theme-initialize)
-(setq color-theme-is-global t)
-;(color-theme-wombat+)
-(color-theme-tangotango)
+;;(add-to-list 'load-path "~/.emacs.d/color-theme")
+;;(add-to-list 'load-path "~/.emacs.d/color-theme/themes")
+;;(require 'color-theme)
+;;(color-theme-initialize)
+;;(setq color-theme-is-global t)
+;;(color-theme-tangotango)
+;;;(color-theme-wombat+)
 
+;;; Color-theme-solarized
+(add-to-list 'load-path "~/.emacs.d/color-theme")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/color-theme/themes")
+(load-theme 'zenburn t)
 (provide 'epy-customize)
